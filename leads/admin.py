1b97731573256f5e63419804c24266f4be8c9ad4
Sunil <skp273213@gmail.com>
2026-06-18 18:26:23 +0530

    fixed admin.py import error

--- leads/admin.py
+++ leads/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Lead, Pincode  # ध्यान दें: यहाँ अपने Pincode मॉडल का असली नाम लिखें जो models.py में है
+from .models import Lead # ध्यान दें: यहाँ अपने Pincode मॉडल का असली नाम लिखें जो models.py में है
 
 admin.site.register(Lead)
-admin.site.register(Pincode)  # यह लाइन पिनकोड को एडमिन पैनल में लाएगी
